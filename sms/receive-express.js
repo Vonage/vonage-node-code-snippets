5b35486231440bcf80c20a75e76ebd236e3334b0
Tomomi ❤ Imura <tomomi.i@gmail.com>
2016-10-12 15:45:05 -0700

    Add sample code for tutorials

---
+++ sms/receive-express.js
@@ -0,0 +1,57 @@
+/* Tutorial 2: Webhook for Nexmo SMS incoming messages demo with ExpressJS */
+
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(3000, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+// Persist data storage to store data in the file system w/o a real DB
+const storage = require('node-persist');
+storage.init();
+
+// Handle both GET and POST requests
+
+app.get('/inbound', (req, res) => {
+  handleQueries(req.query, res);
+});
+
+app.post('/inbound', (req, res) => {
+  handleQueries(req.body, res);
+});
+
+function handleQueries(params, res) {
+  if (!params.to || !params.msisdn) {
+    console.log('This is not a valid inbound SMS message!');
+  } else {
+    console.log('Success');
+    let incomingData = {
+      messageId: params.messageId,
+      from: params.msisdn,
+      text: params.text,
+      type: params.type,
+      timestamp: params['message-timestamp']
+    };
+    storage.setItem('id_' + params.messageId, incomingData);
+    res.send(incomingData);
+  }
+  res.status(200).end();
+}
+
+// Optional: To spit out JSON data for each Message ID
+// e.g. http://localhost:3000/inbound/02000000F8835159
+app.get('/inbound/:id', (req, res) => {
+  try {
+    storage.getItem('id_' + req.params.id).then((value) => {
+      res.json(value);
+    });
+  } catch (e) {
+    res.status(404).end();
+  }
+});
---
+++ sms/send-express.js
@@ -0,0 +1,30 @@
+/* Tutorial 1: Nexmo SMS outcoming messages demo with ExpressJS */
+
+'use strict';
+
+require('dotenv').config({path: __dirname + '/../.env'});
+const API_KEY = process.env.NEXMO_API_KEY;
+const API_SECRET = process.env.NEXMO_API_SECRET;
+const FROM_NUMBER = process.env.EXAMPLE_FROM_NUMBER;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Nexmo = require('nexmo');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const server = app.listen(3000);
+
+const nexmo = new Nexmo({
+  apiKey: API_KEY,
+  apiSecret: API_SECRET,
+}, {debug: true});
+
+app.post('/send', (req, res) => {
+  // Sending SMS via Nexmo
+  nexmo.message.sendSms(
+    FROM_NUMBER, req.body.toNumber, req.body.message, {type: 'unicode'},
+    (err, responseData) => {if (responseData) {console.log(responseData)}}
+  );
+});
