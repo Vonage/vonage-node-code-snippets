4e29d7cfdb2b975ea4e9aa4bcbf87d17450f749a
leggetter <phil@leggetter.co.uk>
2017-06-16 10:46:13 +0100

    Adding command line NEXMO_TO_NUMBER support

--- sms/send.js
+++ sms/send.js
@@ -3,7 +3,9 @@ require('dotenv').config({path: __dirname + '/../.env'})
 const NEXMO_API_KEY = process.env.NEXMO_API_KEY
 const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET
 const NEXMO_FROM_NUMBER = process.env.NEXMO_FROM_NUMBER
-const NEXMO_TO_NUMBER = process.env.NEXMO_TO_NUMBER
+
+// By default use the command line argument. Otherwise use the environment variable.
+const NEXMO_TO_NUMBER = process.argv[2] || process.env.NEXMO_TO_NUMBER
 
 const Nexmo = require('nexmo')
 
