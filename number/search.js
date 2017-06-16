8ce6d63cbc745071bf19a7b63e6c5b5ed1a14605
leggetter <phil@leggetter.co.uk>
2017-06-16 10:37:00 +0100

    Adding “NEXMO_” prefix

--- number/search.js
+++ number/search.js
@@ -1,12 +1,11 @@
 require('dotenv').config({path: __dirname + '/../.env'});
 
-var API_KEY = process.env.NEXMO_API_KEY;
-var API_SECRET = process.env.NEXMO_API_SECRET;
-var TO_NUMBER = process.env.EXAMPLE_TO_NUMBER;
+var NEXMO_API_KEY = process.env.NEXMO_API_KEY;
+var NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
 
 var Nexmo = require('nexmo');
 
-var nexmo = new Nexmo({apiKey: API_KEY, apiSecret: API_SECRET}, {debug: true});
+var nexmo = new Nexmo({apiKey: NEXMO_API_KEY, apiSecret: NEXMO_API_SECRET}, {debug: true});
 
 nexmo.number.search('GB', {features: 'SMS'}, function(err, res) {
   if(err) {
--- sms/send-express.js
+++ sms/send-express.js
@@ -4,6 +4,10 @@
 
 require('dotenv').config({path: __dirname + '/../.env'});
 
+const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
+const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
+const NEXMO_FROM_NUMBER = process.env.NEXMO_FROM_NUMBER;
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const Nexmo = require('nexmo');
@@ -14,14 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(process.env.PORT || 3000);
 
 const nexmo = new Nexmo({
-  apiKey: process.env.API_KEY,
-  apiSecret: process.env.API_SECRET,
+  apiKey: NEXMO_.API_KEY,
+  apiSecret: NEXMO_API_SECRET,
 }, {debug: true});
 
 app.post('/send', (req, res) => {
   // Sending SMS via Nexmo
   nexmo.message.sendSms(
-    process.env.FROM_NUMBER, req.body.toNumber, req.body.message, {type: 'unicode'},
+    NEXMO_FROM_NUMBER, req.body.toNumber, req.body.message, {type: 'unicode'},
     (err, responseData) => {if (responseData) {console.log(responseData)}}
   );
 });
