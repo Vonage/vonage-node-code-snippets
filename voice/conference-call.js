ebb3d848b7b939b661dd5fe228656f1088fc3d1b
leggetter <phil@leggetter.co.uk>
2017-06-23 20:10:10 +0100

    conference call example

---
+++ voice/conference-call.js
@@ -0,0 +1,34 @@
+'use strict';
+
+require('dotenv').config({path: __dirname + '/../.env'});
+
+const NEXMO_FROM_NUMBER = process.env.NEXMO_FROM_NUMBER;
+const NEXMO_TO_NUMBER = process.env.NEXMO_TO_NUMBER;
+
+const app = require('express')();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = app.listen(process.env.PORT || 4004, () => {
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
+
+app.get('/answer', (req, res) => {
+  const ncco = [
+    {
+      action: 'talk',
+      text: 'Welcome to a Nexmo powered conference call'
+    },
+    {
+      action: 'conversation',
+      name: 'nexmo-conference'
+    }
+  ];
+  res.json(ncco);
+});
+
+app.post('/events', (req, res) => {
+  console.log(req.body);
+  res.status(204).end();
+});
