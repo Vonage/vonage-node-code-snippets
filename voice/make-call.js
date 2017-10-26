4d88e2d2a09ed3de13885228698fdfdc427fc6e4
Adam Butler <adam@lab.io>
2017-10-26 12:27:12 +0100

    Disable debug

--- voice/make-call.js
+++ voice/make-call.js
@@ -14,9 +14,7 @@ const nexmo = new Nexmo({
   apiKey: NEXMO_API_KEY,
   apiSecret: NEXMO_API_SECRET,
   applicationId: NEXMO_APPLICATION_ID,
-  privateKey: NEXMO_APPLICATION_PRIVATE_KEY_PATH,
-}, {
-  debug: true
+  privateKey: NEXMO_APPLICATION_PRIVATE_KEY_PATH
 })
 
 nexmo.calls.create({
