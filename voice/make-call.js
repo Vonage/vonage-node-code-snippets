1b84c22ed45f8d54675d30dd44da324f739674e2
Adam Butler <adam@lab.io>
2017-10-26 12:26:23 +0100

    Simplify make a call example

--- voice/make-call.js
+++ voice/make-call.js
@@ -1,37 +1,23 @@
-/* Voice Tutorial 1: Using Nexmo API to call your phone
-   Before running this code, you must create an Application using the Nexmo CLI.
-   Then run this node code with a phone number starts with an country code as the argument.
-   e.g. Run it with a US number, start with the country code 1:
-   $ node make-call.js 12025559999
+require('dotenv').config({path: __dirname + '/../.env'})
 
-   API Referene: https://docs.nexmo.com/voice/voice-api/api-reference
- */
+const NEXMO_TO_NUMBER = process.env.NEXMO_TO_NUMBER
+const NEXMO_FROM_NUMBER = process.env.NEXMO_FROM_NUMBER
 
-'use strict';
+const NEXMO_API_KEY = process.env.NEXMO_API_KEY
+const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET
+const NEXMO_APPLICATION_ID = process.env.NEXMO_APPLICATION_ID
+const NEXMO_APPLICATION_PRIVATE_KEY_PATH = __dirname +"/../"+ process.env.NEXMO_APPLICATION_PRIVATE_KEY_PATH
 
-require('dotenv').config({path: __dirname + '/../.env'});
-
-const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
-const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
-const NEXMO_PRIVATE_KEY = __dirname +"/../"+ process.env.NEXMO_PRIVATE_KEY;
-const NEXMO_APPLICATION_ID = process.env.NEXMO_APPLICATION_ID;
-
-const NEXMO_TO_NUMBER = process.argv[2];
-const NEXMO_FROM_NUMBER = process.env.NEXMO_FROM_NUMBER;
-
-if(!NEXMO_TO_NUMBER) {
-  console.error('Please provide a number to call as a command line argument');
-  return;
-}
-
-const Nexmo = require('nexmo');
+const Nexmo = require('nexmo')
 
 const nexmo = new Nexmo({
   apiKey: NEXMO_API_KEY,
   apiSecret: NEXMO_API_SECRET,
   applicationId: NEXMO_APPLICATION_ID,
-  privateKey: NEXMO_PRIVATE_KEY
-});
+  privateKey: NEXMO_APPLICATION_PRIVATE_KEY_PATH,
+}, {
+  debug: true
+})
 
 nexmo.calls.create({
   to: [{
@@ -42,8 +28,5 @@ nexmo.calls.create({
     type: 'phone',
     number: NEXMO_FROM_NUMBER
   },
-  answer_url: ['https://nexmo-community.github.io/ncco-examples/first_call_talk.json']
-}, (err, res) => {
-  if(err) { console.error(err); }
-  else { console.log(res); }
-});
+  answer_url: ['https://developer.nexmo.com/ncco/tts.json']
+})
