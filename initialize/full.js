1d16a516512fd08f77e381de05f611bb88b0c78c
Adam Butler <adam@lab.io>
2017-10-27 17:30:50 +0100

    Add initialize JS files

---
+++ initialize/full.js
@@ -0,0 +1,8 @@
+const Nexmo = require('nexmo')
+
+const nexmo = new Nexmo({
+  apiKey: NEXMO_API_KEY,
+  apiSecret: NEXMO_API_SECRET,
+  applicationId: NEXMO_APPLICATION_ID,
+  privateKey: NEXMO_APPLICATION_PRIVATE_KEY_PATH
+})
