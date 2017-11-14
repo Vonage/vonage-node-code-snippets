72c21e5f12040946c5b531162957d04cdbceba15
Adam Butler <adam@lab.io>
2017-11-14 16:52:57 +0000

    Simplify receive sms example

--- sms/receive-express.js
+++ sms/receive-express.js
@@ -1,57 +1,18 @@
-/* Tutorial 2: Webhook for Nexmo SMS incoming messages demo with ExpressJS */
+const app = require('express')()
+const bodyParser = require('body-parser')
 
-'use strict';
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app
+  .route('/webhooks/inbound-sms')
+  .get(handleInboundSms)
+  .post(handleInboundSms)
 
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
-});
-
-// Persist data storage to store data in the file system w/o a real DB
-const storage = require('node-persist');
-storage.init();
-
-// Handle both GET and POST requests
-
-app.get('/inbound', (req, res) => {
-  handleParams(req.query, res);
-});
-
-app.post('/inbound', (req, res) => {
-  handleParams(req.body, res);
-});
-
-function handleParams(params, res) {
-  if (!params.to || !params.msisdn) {
-    console.log('This is not a valid inbound SMS message!');
-  } else {
-    console.log('Success');
-    let incomingData = {
-      messageId: params.messageId,
-      from: params.msisdn,
-      text: params.text,
-      type: params.type,
-      timestamp: params['message-timestamp']
-    };
-    storage.setItem('id_' + params.messageId, incomingData);
-    res.send(incomingData);
-  }
-  res.status(200).end();
+function handleInboundSms(request, response) {
+  const params = Object.assign(request.query, request.body)
+  console.log(params)
+  response.status(204).send()
 }
 
-// Optional: To spit out JSON data for each Message ID
-// e.g. http://localhost:3000/inbound/02000000F8835159
-app.get('/inbound/:id', (req, res) => {
-  try {
-    storage.getItem('id_' + req.params.id).then((value) => {
-      res.json(value);
-    });
-  } catch (e) {
-    res.status(404).end();
-  }
-});
+app.listen(3000)
