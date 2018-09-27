aaaf83ae8178bc3a149cd908d62889a271c196fb
Alex Lakatos <alex.lakatos.qa@gmail.com>
2018-09-27 12:18:58 +0100

    merge master

---
+++ channels/send-whatsapp.js
@@ -0,0 +1,30 @@
+require('dotenv').config({path: __dirname + '/../.env'})
+
+const TO_NUMBER = process.env.TO_NUMBER
+const NEXMO_NUMBER = process.env.NEXMO_NUMBER
+
+const NEXMO_API_KEY = process.env.NEXMO_API_KEY
+const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET
+const NEXMO_APPLICATION_ID = process.env.NEXMO_APPLICATION_ID
+const NEXMO_APPLICATION_PRIVATE_KEY_PATH = __dirname +"/../"+ process.env.NEXMO_APPLICATION_PRIVATE_KEY_PATH
+
+const Nexmo = require('nexmo')
+
+const nexmo = new Nexmo({
+  apiKey: NEXMO_API_KEY,
+  apiSecret: NEXMO_API_SECRET,
+  applicationId: NEXMO_APPLICATION_ID,
+  privateKey: NEXMO_APPLICATION_PRIVATE_KEY_PATH
+})
+
+nexmo.channel.send(
+  { "type": "whatsapp", "number": "TO_NUMBER" },
+  { "type": "whatsapp", "number": "WHATSAPP_NUMBER" },
+  {
+    "content": {
+      "type": "text",
+      "text": "This is a WhatsApp Message sent from the Messages API"
+    }
+  },
+  (err, data) => { console.log(data.message_uuid); }
+);
