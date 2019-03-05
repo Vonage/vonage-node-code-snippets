28c0edd29c9560b27212353beb6040ba15dcf307
Mark Lewin <mark.lewin@vonage.com>
2019-03-05 10:24:50 +0000

    Add VBC extension example

---
+++ vbc/connect-to-extension.js
@@ -0,0 +1,20 @@
+require('dotenv').config({path: __dirname + '/../.env'});
+const VBC_EXTENSION = process.env.VBC_EXTENSION;
+
+const app = require('express')()
+
+const onInboundCall = (request, response) => {
+  const ncco = [{
+    action: 'connect',
+    endpoint: [{
+      type: 'vbc',
+      number: VBC_EXTENSION
+    }]
+  }]
+
+  response.json(ncco)
+}
+
+app.get('/webhooks/answer', onInboundCall)
+
+app.listen(3000)
