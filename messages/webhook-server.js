ee536250874e03539aa0f9e0b3d372591124c945
Tony Bedford <tony.bedford@vonage.com>
2019-08-09 13:02:55 +0100

    Add webhook server

---
+++ messages/webhook-server.js
@@ -0,0 +1,19 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/webhooks/inbound-message', (req, res) => {
+  console.log(req.body);
+  res.status(200).end();
+});
+
+app.post('/webhooks/message-status', (req, res) => {
+  console.log(req.body);
+  res.status(200).end();
+});
+
+app.listen(3000)
