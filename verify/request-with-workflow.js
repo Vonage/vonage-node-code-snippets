258a4a36b60c6578b53907eb3688b904245cf237
Alex Lakatos <alex.lakatos.qa@gmail.com>
2019-08-27 17:56:42 +0800

    add veriify request with workflow

---
+++ verify/request-with-workflow.js
@@ -0,0 +1,28 @@
+require('dotenv').config({
+  path: __dirname + '/../.env'
+});
+
+const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
+const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
+const RECIPIENT_NUMBER = process.env.RECIPIENT_NUMBER;
+const NEXMO_BRAND_NAME = process.env.NEXMO_BRAND_NAME;
+const WORKFLOW_ID = process.env.WORKFLOW_ID;
+
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
+  apiKey: NEXMO_API_KEY,
+  apiSecret: NEXMO_API_SECRET
+});
+
+nexmo.verify.request({
+  number: RECIPIENT_NUMBER,
+  brand: NEXMO_BRAND_NAME,
+  workflow_id: WORKFLOW_ID
+}, (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const verifyRequestId = result.request_id;
+    console.log('request_id', verifyRequestId);
+  }
+});
