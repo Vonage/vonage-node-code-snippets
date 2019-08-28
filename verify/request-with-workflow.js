3680b5f9bc3dc474f6801b5bfee64d43a6ffdd26
Alex Lakatos <alex.lakatos.qa@gmail.com>
2019-08-28 13:07:04 +0800

    update BRAND_NAME

--- verify/request-with-workflow.js
+++ verify/request-with-workflow.js
@@ -5,7 +5,7 @@ require('dotenv').config({
 const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
 const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
 const RECIPIENT_NUMBER = process.env.RECIPIENT_NUMBER;
-const NEXMO_BRAND_NAME = process.env.NEXMO_BRAND_NAME;
+const BRAND_NAME = process.env.BRAND_NAME;
 const WORKFLOW_ID = process.env.WORKFLOW_ID;
 
 const Nexmo = require('nexmo');
@@ -16,7 +16,7 @@ const nexmo = new Nexmo({
 
 nexmo.verify.request({
   number: RECIPIENT_NUMBER,
-  brand: NEXMO_BRAND_NAME,
+  brand: BRAND_NAME,
   workflow_id: WORKFLOW_ID
 }, (err, result) => {
   if (err) {
