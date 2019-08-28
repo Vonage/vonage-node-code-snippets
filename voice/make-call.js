416b183146a29b0f0871df537c8d0c44861793f7
Alex Lakatos <alex.lakatos.qa@gmail.com>
2019-08-28 11:53:53 +0800

    revert TO_NUMBER

--- voice/make-call.js
+++ voice/make-call.js
@@ -1,6 +1,6 @@
 require('dotenv').config({path: __dirname + '/../.env'})
 
-const NEXMO_TO_NUMBER = process.env.NEXMO_TO_NUMBER
+const TO_NUMBER = process.env.TO_NUMBER
 const NEXMO_NUMBER = process.env.NEXMO_NUMBER
 
 const NEXMO_API_KEY = process.env.NEXMO_API_KEY
@@ -20,7 +20,7 @@ const nexmo = new Nexmo({
 nexmo.calls.create({
   to: [{
     type: 'phone',
-    number: NEXMO_TO_NUMBER
+    number: TO_NUMBER
   }],
   from: {
     type: 'phone',
