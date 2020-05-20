5512c4f7126c948a288657fbd9dc2f231795e91c
niati <nick.iati@vonage.com>
2020-05-20 13:41:13 -0400

    Updated with code for validating payload hash

--- messages/signed-webhooks/verify-signed-webhook.js
+++ messages/signed-webhooks/verify-signed-webhook.js
@@ -1,3 +1,8 @@
 const jwt = require("jsonwebtoken")
 
+const sha256 = require('js-sha256');
+if (sha256(payload) != claims["payload_hash"]) {
+ // payload has been tampered with
+}
+
 var decoded = jwt.verify(signature,'SECRET')
