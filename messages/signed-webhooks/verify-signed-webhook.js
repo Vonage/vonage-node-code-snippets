e634b6a9140a3f9a87381c7f92e8bcf645cca175
niati <nick.iati@vonage.com>
2020-05-21 13:18:55 -0400

    Updated code snippet

--- messages/signed-webhooks/verify-signed-webhook.js
+++ messages/signed-webhooks/verify-signed-webhook.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken")
-
 const sha256 = require('js-sha256');
-if (sha256(payload) != claims["payload_hash"]) {
- // payload has been tampered with
-}
 
 var decoded = jwt.verify(signature,'SECRET')
+if (sha256(payload) != claims["payload_hash"]) {
+    // payload has been tampered with
+   }
