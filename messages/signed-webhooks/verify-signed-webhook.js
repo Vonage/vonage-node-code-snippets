bef702b20b20040e9d5e8b673b0756653f88ab5e
niati <nick.iati@vonage.com>
2020-05-21 13:29:50 -0400

    Updated code replacing decode with claims

--- messages/signed-webhooks/verify-signed-webhook.js
+++ messages/signed-webhooks/verify-signed-webhook.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken")
 const sha256 = require('js-sha256');
 
-var decoded = jwt.verify(signature,'SECRET')
+var claims = jwt.verify(signature,'SECRET')
 if (sha256(payload) != claims["payload_hash"]) {
     // payload has been tampered with
    }
