01603e6550812f1448e3cfdcbb79f1c490b4116c
Mark Lewin <mark.lewin@vonage.com>
2020-06-24 11:16:56 +0100

    Add send Viber image snippet

---
+++ messages/viber/send-image.js
@@ -0,0 +1,46 @@
+require('dotenv').config({ path: __dirname + '/../../.env' });
+
+const NEXMO_API_KEY = process.env.NEXMO_API_KEY;
+const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET;
+const NEXMO_APPLICATION_ID = process.env.NEXMO_APPLICATION_ID;
+const NEXMO_APPLICATION_PRIVATE_KEY_PATH =
+	__dirname + '/../../' + process.env.NEXMO_APPLICATION_PRIVATE_KEY_PATH;
+
+const TO_NUMBER = process.env.TO_NUMBER;
+const VIBER_SERVICE_MESSAGE_ID = process.env.VIBER_SERVICE_MESSAGE_ID;
+const BASE_URL = process.env.BASE_URL;
+const IMAGE_URL = process.env.IMAGE_URL;
+
+const Nexmo = require('nexmo');
+
+const nexmo = new Nexmo(
+	{
+		apiKey: NEXMO_API_KEY,
+		apiSecret: NEXMO_API_SECRET,
+		applicationId: NEXMO_APPLICATION_ID,
+		privateKey: NEXMO_APPLICATION_PRIVATE_KEY_PATH,
+	},
+	{
+		apiHost: BASE_URL,
+	}
+);
+
+nexmo.channel.send(
+	{ type: 'viber_service_msg', number: TO_NUMBER },
+	{ type: 'viber_service_msg', id: VIBER_SERVICE_MESSAGE_ID },
+	{
+		content: {
+			type: 'image',
+			image: {
+				url: IMAGE_URL,
+			},
+		},
+	},
+	(err, data) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log(data.message_uuid);
+		}
+	}
+);
