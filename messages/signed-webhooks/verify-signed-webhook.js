e121ca9fd3bc543879ae4a7d35655e8a63ec0dbd
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-08-21 19:07:33 +0000

    docs: updating signed sms and messages examples

--- messages/signed-webhooks/verify-signed-webhook.js
+++ messages/signed-webhooks/verify-signed-webhook.js
@@ -1,8 +1,12 @@
-require('dotenv').config({ path: __dirname + '/../.env' });
+require('dotenv').config({ path: __dirname + '/../../.env' });
+const { verifySignature } = require('@vonage/jwt');
+const { readFileSync } = require('fs');
 
-const VONAGE_API_SIGNATURE_SECRET = process.env.VONAGE_API_SIGNATURE_SECRET || '';
-const jwt = require("jsonwebtoken");
-const sha256 = require('js-sha256');
+const VONAGE_APPLICATION_PRIVATE_KEY_PATH = __dirname + "/../../" + process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH;
+
+
+const privateKey = readFileSync(VONAGE_APPLICATION_PRIVATE_KEY_PATH);
+
 const app = require('express')();
 const bodyParser = require('body-parser');
 
@@ -14,28 +18,14 @@ app.use(bodyParser.urlencoded({
 app
   .route('/webhooks/inbound-message')
   .post(handleInboundMessage);
-function handleInboundMessage(request, response){
-  const payload = Object.assign(request.query, request.body);
-  const token = request.headers.authorization.split(" ")[1];
-  try{
-    const decoded = jwt.verify(
-      token,
-      VONAGE_API_SIGNATURE_SECRET,
-      { algorithms:['HS256'] },
-    );
 
-    if(sha256(JSON.stringify(payload))!=decoded["payload_hash"]){
-      console.log("tampering detected");
-      response.status(401).send();
-    }
-    else{
-      console.log("Success");
-      response.status(204).send();
-    }
-  }
-  catch(err){
-    console.log('Bad token detected');
-    response.status(401).send();
+function handleInboundMessage(request, response){
+  const token = request.headers.authorization.split(" ")[1];
+  if (verifySignature(token, privateKey)) {
+    console.log("Valid signature");
+  } else {
+    console.log("Invalid signature");
   }
 }
+
 app.listen(process.env.PORT || 3000);
