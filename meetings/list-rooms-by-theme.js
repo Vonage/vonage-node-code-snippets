ac086377019d3fec96f7e1e66545828ee1ab2059
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-10-31 19:55:14 +0000

    feat: added snippets for meetings

---
+++ meetings/list-rooms-by-theme.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: __dirname + '/../../.env' });
+
+const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+const VONAGE_APPLICATION_PRIVATE_KEY_PATH = __dirname + "/../../" + process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH;
+const THEME_ID = process.env.THEME_ID;
+
+const { Auth } = require('@vonage/auth');
+const { Meetings } = require('@vonage/meetings');
+
+const credentials = new Auth({
+  privateKey: VONAGE_APPLICATION_PRIVATE_KEY_PATH,
+  applicationId: VONAGE_APPLICATION_ID,
+});
+const options = {};
+
+const meetingsClient = new Meetings(credentials, options);
+for (const room of meetingsClient.getRoomsForTheme(THEME_ID)) {
+  console.log(room);
+}
