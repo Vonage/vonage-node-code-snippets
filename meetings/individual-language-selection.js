d2f4d2eff8a5ed2b71fa1f49a016655d2db01497
Chuck Reeves <manchuck@users.noreply.github.com>
2023-11-12 14:29:51 -0500

    feat: added snippets for meetings (#135)

---
+++ meetings/individual-language-selection.js
@@ -0,0 +1,23 @@
+require('dotenv').config({ path: __dirname + '/../../.env' });
+
+const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+const VONAGE_APPLICATION_PRIVATE_KEY_PATH = __dirname + "/../../" + process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH;
+const ROOM_DISPLAY_NAME = process.env.ROOM_DISPLAY_NAME;
+
+const { Auth } = require('@vonage/auth');
+const { Meetings, MeetingType } = require('@vonage/meetings');
+
+const credentials = new Auth({
+  privateKey: VONAGE_APPLICATION_PRIVATE_KEY_PATH,
+  applicationId: VONAGE_APPLICATION_ID,
+});
+const options = {};
+
+const meetingsClient = new Meetings(credentials, options);
+await meetingsClient.createRoom({
+  type: MeetingType.INSTANT,
+  displayName: ROOM_DISPLAY_NAME,
+  availableFeatures: {
+    isLocaleSwitcherAvailable: true,
+  },
+});
---
+++ meetings/retrieve-room.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: __dirname + '/../../.env' });
+
+const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+const VONAGE_APPLICATION_PRIVATE_KEY_PATH = __dirname + "/../../" + process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH;
+const ROOM_ID = process.env.ROOM_ID;
+
+const { Auth } = require('@vonage/auth');
+const { Meetings } = require('@vonage/meetings');
+
+const credentials = new Auth({
+  privateKey: VONAGE_APPLICATION_PRIVATE_KEY_PATH,
+  applicationId: VONAGE_APPLICATION_ID,
+});
+const options = {};
+
+const meetingsClient = new Meetings(credentials, options);
+const room = await meetingsClient.getRoom(ROOM_ID);
+console.log(room);
