73e5196094900c90dadd8ae847efa5af62ca442e
Guillaume Faas <guillaume.faas@live.fr>
2023-11-14 13:51:31 +0100

    Fix retrieve recording snippet

--- meetings/retrieve-recording.js
+++ meetings/retrieve-recording.js
@@ -2,7 +2,7 @@ require('dotenv').config({ path: __dirname + '/../../.env' });
 
 const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
 const VONAGE_APPLICATION_PRIVATE_KEY_PATH = __dirname + "/../../" + process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH;
-const SESSION_ID = process.env.SESSION_ID;
+const RECORDING_ID = process.env.RECORDING_ID;
 
 const { Auth } = require('@vonage/auth');
 const { Meetings } = require('@vonage/meetings');
@@ -14,6 +14,4 @@ const credentials = new Auth({
 const options = {};
 
 const meetingsClient = new Meetings(credentials, options);
-for await (const recording of meetingsClient.getSessionRecordings(SESSION_ID)) {
-  console.log(recording);
-}
+const recording = await meetingsClient.getRecording(RECORDING_ID);
