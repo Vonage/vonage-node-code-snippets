3c8f78b7038f48f09fd2f47c434c7d07238dce8e
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2023-11-21 16:11:58 +0000

    feat(voice): updating file download snippet

--- voice/download-a-recording.js
+++ voice/download-a-recording.js
@@ -1,24 +1,19 @@
-'use strict';
+require('dotenv').config({ path: __dirname + '/../.env' });
 
-require('dotenv').config({path: __dirname + '/../.env'});
-
-const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
-const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
 const VONAGE_PRIVATE_KEY = __dirname +"/../"+ process.env.VONAGE_PRIVATE_KEY;
 const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+const RECORDING_URL = process.env.RECORDING_URL;
 
-const { Vonage } = require('@vonage/server-sdk');
+const { FileClient } = require('@vonage/server-client');
 
-const vonage = new Vonage({
-  apiKey: VONAGE_API_KEY,
-  apiSecret: VONAGE_API_SECRET,
+const fileClient = new FileClient({
   applicationId: VONAGE_APPLICATION_ID,
-  privateKey: VONAGE_PRIVATE_KEY
-}, {debug: true});
-
-vonage.files.save(RECORDING_URL, 'test.mp3', (err, res) => {
-  if(err) { console.error(err); }
-  else {
-      console.log(res);
-  }
+  privateKey: VONAGE_PRIVATE_KEY,
 });
+
+// Save the file to a specific location
+await fileClient.downloadFile(
+  RECORDING_URL, 
+  '/path/to/save/file.mp3',
+);
+
