2bb4a331b5bb7342eaf9c3ac73057fa3a7f72eac
Chuck Reeves <manchuck@users.noreply.github.com>
2024-01-11 14:33:24 -0500

    fix(sms): example for sending sms is incorrect (#141)

--- sms/send-signed-sms.js
+++ sms/send-signed-sms.js
@@ -1,32 +1,39 @@
-require('dotenv').config({path: __dirname + '/../.env'})
+require('dotenv').config({ path: __dirname + '/../.env' });
 
-const VONAGE_API_KEY = process.env.VONAGE_API_KEY
-const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET
-const VONAGE_API_SIGNATURE_SECRET = process.env.VONAGE_API_SIGNATURE_SECRET
-const TO_NUMBER = process.env.VONAGE_TO_NUMBER
-const FROM_NUMBER = process.env.VONAGE_FROM_NUMBER
+const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
+const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
+const VONAGE_API_SIGNATURE_SECRET = process.env.VONAGE_API_SIGNATURE_SECRET;
+const TO_NUMBER = process.env.VONAGE_TO_NUMBER;
+const FROM_NUMBER = process.env.VONAGE_FROM_NUMBER;
 
-const { Vonage } = require('@vonage/server-sdk')
+const { Vonage } = require('@vonage/server-sdk');
 
 const vonage = new Vonage({
   apiKey: VONAGE_API_KEY,
   apiSecret: VONAGE_API_SECRET,
-  signatureSecret: VONAGE_API_SIGNATURE_SECRET,
-  signatureMethod: "md5hash"
-})
+  signature: {
+    secret: VONAGE_API_SIGNATURE_SECRET,
+    algorithm: 'md5hash',
+  },
+});
 
-const from = FROM_NUMBER
-const to = TO_NUMBER
-const text = 'A text message sent using the Vonage SMS API'
+const from = FROM_NUMBER;
+const to = TO_NUMBER;
+const text = 'A text message sent using the Vonage SMS API';
 
-vonage.message.sendSms(from, to, text, (err, responseData) => {
-    if (err) {
-        console.log(err);
-    } else {
-        if(responseData.messages[0]['status'] === "0") {
-            console.log("Message sent successfully.");
-        } else {
-            console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
-        }
-    }
-})
+const sendMessage = async () => {
+  try {
+    const result = await vonage.sms.send({
+      from: from,
+      to: to,
+      text: text,
+    });
+
+    console.log("Message sent successfully.");
+    console.log(result);
+  } catch (error) {
+    console.error(`Message failed with error: ${error.message}`);
+  }
+};
+
+sendMessage();
