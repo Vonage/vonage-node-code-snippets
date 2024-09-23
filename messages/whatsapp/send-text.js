9a3e318f2e11a8fecf855562979276751fe384e6
Chuck Reeves <manchuck@users.noreply.github.com>
2024-09-23 07:49:55 -0400

    docs: removed apiKey and apiSecret from whats app

--- messages/whatsapp/send-text.js
+++ messages/whatsapp/send-text.js
@@ -1,7 +1,5 @@
 require('dotenv').config({ path: __dirname + '/../../.env' });
 
-const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
-const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
 const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
 const VONAGE_PRIVATE_KEY = process.env.VONAGE_PRIVATE_KEY;
 
@@ -12,8 +10,6 @@ const { Vonage } = require('@vonage/server-sdk');
 const { WhatsAppText } = require('@vonage/messages');
 
 const vonage = new Vonage({
-  apiKey: VONAGE_API_KEY,
-  apiSecret: VONAGE_API_SECRET,
   applicationId: VONAGE_APPLICATION_ID,
   privateKey: VONAGE_PRIVATE_KEY,
 });
