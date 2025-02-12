9a825e3ccbba875b931a70d4130b7772abfaba49
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-02-12 14:05:01 -0500

    refactor: update application snippets

--- application/delete-application.js
+++ application/delete-application.js
@@ -1,16 +1,15 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
+const { Vonage } = require('@vonage/server-sdk');
 
 const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
 const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
 const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
-
-const { Vonage } = require('@vonage/server-sdk');
 
 const vonage = new Vonage({
   apiKey: VONAGE_API_KEY,
   apiSecret: VONAGE_API_SECRET,
 });
 
-vonage.applications.getApplication(VONAGE_APPLICATION_ID)
+vonage.applications.deleteApplication(VONAGE_APPLICATION_ID)
   .then((resp) => console.log(resp))
   .catch((error) => console.error(error));
