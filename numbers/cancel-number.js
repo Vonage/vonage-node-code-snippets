98b4602757e33fc211a79ba15de56e58762c972c
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-02-19 11:31:50 -0500

    refactor: updated numbers and number-insights

--- numbers/cancel-number.js
+++ numbers/cancel-number.js
@@ -1,18 +1,19 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
+const { Vonage } = require('@vonage/server-sdk');
 
 const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
 const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
-
-// By default use the command line argument. Otherwise use the environment variable.
-const INSIGHT_NUMBER = process.argv[2] || process.env.INSIGHT_NUMBER;
-
-const { Vonage } = require('@vonage/server-sdk');
+const VONAGE_NUMBER = process.env.VONAGE_NUMBER;
+const COUNTRY_CODE = process.env.COUNTRY_CODE;
 
 const vonage = new Vonage({
   apiKey: VONAGE_API_KEY,
   apiSecret: VONAGE_API_SECRET,
 });
 
-vonage.numberInsights.basicLookup(INSIGHT_NUMBER)
+vonage.number.cancelNumber({
+  country: COUNTRY_CODE,
+  msisdn: VONAGE_NUMBER,
+})
   .then((result) => console.log(result))
   .catch((error) => console.error(error));
