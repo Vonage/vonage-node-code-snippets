d42733008eb060e7ccb8febbcec578e4ff64abef
Chuck Reeves <manchuck@users.noreply.github.com>
2025-02-24 14:18:06 -0500

    refactor: update application snippets (#170)

---
+++ application/update-application.js
@@ -0,0 +1,61 @@
+require('dotenv').config({ path: __dirname + '/../.env' });
+const { Vonage } = require('@vonage/server-sdk');
+
+const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
+const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
+const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+
+const vonage = new Vonage({
+  apiKey: VONAGE_API_KEY,
+  apiSecret: VONAGE_API_SECRET,
+});
+
+vonage.applications.updateApplication({
+  id: VONAGE_APPLICATION_ID,
+  name: "New App Name",
+  capabilities: {
+    messages: {
+      webhooks: {
+        inboundUrl: {
+          address: "https://example.com/webhooks/inbound",
+          httpMethod: "POST"
+        },
+        statusUrl: {
+          address: "https://example.com/webhooks/status",
+          httpMethod: "POST"
+        }
+      }
+    },
+    voice: {
+      webhooks: {
+        answerUrl: {
+          address: "https://example.com/webhooks/answer",
+          httpMethod: "POST"
+        },
+        eventUrl: {
+          address: "https://example.com/webhooks/event",
+          httpMethod: "POST"
+        }
+      }
+    },
+    rtc: {
+      webhooks: {
+        eventUrl: {
+          address: "https://example.com/webhooks/event",
+          httpMethod: "POST"
+        }
+      }
+    },
+    vbc: {},
+    verify: {
+      webhooks: {
+        statusUrl: {
+          address: "https://example.com/webhooks/status",
+          httpMethod: "POST"
+        }
+      }
+    }
+  }
+})
+  .then((resp) => console.log(resp))
+  .catch((error) => console.error(error));
