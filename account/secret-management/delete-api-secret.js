2a55eba6f57d21fdcdfd621d1afb7f47e816aa26
Chuck Reeves <manchuck@users.noreply.github.com>
2025-02-24 14:17:50 -0500

    refactor: updated account snippets (#171)

--- account/secret-management/delete-api-secret.js
+++ account/secret-management/delete-api-secret.js
@@ -1,16 +1,16 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
+const { Vonage } = require('@vonage/server-sdk');
 
 const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
 const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
-const VONAGE_SECRET_ID = process.env.VONAGE_SECRET_ID;
-
-const { Vonage } = require('@vonage/server-sdk');
+const ACCOUNT_SECRET_ID = process.env.VONAGE_SECRET_ID;
+const ACCOUNT_ID = process.env.ACCOUNT_ID;
 
 const vonage = new Vonage({
   apiKey: VONAGE_API_KEY,
   apiSecret: VONAGE_API_SECRET,
 });
 
-vonage.secrets.getSecret(VONAGE_API_KEY, VONAGE_SECRET_ID)
-  .then((resp) => console.log(resp))
+vonage.accounts.deleteSecret(ACCOUNT_ID, ACCOUNT_SECRET_ID)
+  .then(() => console.log('Secret deleted'))
   .catch((error) => console.error(error));
