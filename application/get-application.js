ab653324c4f3a0ce87b6562fe3a2cfa35e042bf7
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-03-06 12:59:03 -0500

    refactor: updated application imports

--- application/get-application.js
+++ application/get-application.js
@@ -1,9 +1,10 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
-const { Vonage } = require('@vonage/server-sdk');
 
 const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
 const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
 const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+
+const { Vonage } = require('@vonage/server-sdk');
 
 const vonage = new Vonage({
   apiKey: VONAGE_API_KEY,
--- application/update-application.js
+++ application/update-application.js
@@ -1,9 +1,10 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
-const { Vonage } = require('@vonage/server-sdk');
 
 const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
 const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
 const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+
+const { Vonage } = require('@vonage/server-sdk');
 
 const vonage = new Vonage({
   apiKey: VONAGE_API_KEY,
@@ -12,50 +13,50 @@ const vonage = new Vonage({
 
 vonage.applications.updateApplication({
   id: VONAGE_APPLICATION_ID,
-  name: "New App Name",
+  name: 'New App Name',
   capabilities: {
     messages: {
       webhooks: {
         inboundUrl: {
-          address: "https://example.com/webhooks/inbound",
-          httpMethod: "POST"
+          address: 'https://example.com/webhooks/inbound',
+          httpMethod: 'POST',
         },
         statusUrl: {
-          address: "https://example.com/webhooks/status",
-          httpMethod: "POST"
-        }
-      }
+          address: 'https://example.com/webhooks/status',
+          httpMethod: 'POST',
+        },
+      },
     },
     voice: {
       webhooks: {
         answerUrl: {
-          address: "https://example.com/webhooks/answer",
-          httpMethod: "POST"
+          address: 'https://example.com/webhooks/answer',
+          httpMethod: 'POST',
         },
         eventUrl: {
-          address: "https://example.com/webhooks/event",
-          httpMethod: "POST"
-        }
-      }
+          address: 'https://example.com/webhooks/event',
+          httpMethod: 'POST',
+        },
+      },
     },
     rtc: {
       webhooks: {
         eventUrl: {
-          address: "https://example.com/webhooks/event",
-          httpMethod: "POST"
-        }
-      }
+          address: 'https://example.com/webhooks/event',
+          httpMethod: 'POST',
+        },
+      },
     },
     vbc: {},
     verify: {
       webhooks: {
         statusUrl: {
-          address: "https://example.com/webhooks/status",
-          httpMethod: "POST"
-        }
-      }
-    }
-  }
+          address: 'https://example.com/webhooks/status',
+          httpMethod: 'POST',
+        },
+      },
+    },
+  },
 })
   .then((resp) => console.log(resp))
   .catch((error) => console.error(error));
