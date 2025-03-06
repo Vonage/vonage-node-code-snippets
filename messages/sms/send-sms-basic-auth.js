0719fca486df9f8a221a238ac8e8fd68992b7c1d
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-03-06 13:12:11 -0500

    refactor: updated sms imports

--- messages/sms/send-sms-basic-auth.js
+++ messages/sms/send-sms-basic-auth.js
@@ -1,12 +1,13 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
-const { Vonage } = require('@vonage/server-sdk');
-const { Channels } = require('@vonage/messages');
 
 const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
 const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
 const MESSAGES_TO_NUMBER = process.env.MESSAGES_TO_NUMBER;
 const SMS_SEENDER_ID = process.env.SMS_SEENDER_ID;
 const MESSAGES_API_URL = process.env.MESSAGES_API_URL;
+
+const { Vonage } = require('@vonage/server-sdk');
+const { Channels } = require('@vonage/messages');
 
 /**
  * By leaving out the applicationId and privateKey parameters, the Vonage
