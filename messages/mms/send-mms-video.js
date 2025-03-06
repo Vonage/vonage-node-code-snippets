4b6c22648e26c541e08dd34764956ad2efb27618
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-03-06 13:15:59 -0500

    refactor: updated mms imports

--- messages/mms/send-mms-video.js
+++ messages/mms/send-mms-video.js
@@ -1,6 +1,4 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
-const { Vonage } = require('@vonage/server-sdk');
-const { Channels } = require('@vonage/messages');
 
 const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
 const VONAGE_PRIVATE_KEY = process.env.VONAGE_PRIVATE_KEY;
@@ -8,6 +6,9 @@ const MESSAGES_TO_NUMBER = process.env.MESSAGES_TO_NUMBER;
 const MMS_SEENDER_ID = process.env.MMS_SEENDER_ID;
 const MESSAGES_VIDEO_URL = process.env.MESSAGES_VIDEO_URL;
 const MESSAGES_API_URL = process.env.MESSAGES_API_URL;
+
+const { Vonage } = require('@vonage/server-sdk');
+const { Channels } = require('@vonage/messages');
 
 /**
  * It is best to send messages using JWT instead of basic auth. If you leave out
