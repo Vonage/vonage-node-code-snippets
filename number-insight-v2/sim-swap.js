c247b1d28ebcfb30d1c627dd915d4449e5a12f91
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2025-03-06 13:25:58 -0500

    refactor: updated number-insights-v2 imports

--- number-insight-v2/sim-swap.js
+++ number-insight-v2/sim-swap.js
@@ -1,9 +1,10 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
-const { NumberInsightV2, Insight } = require('@vonage/number-insight-v2');
 
 const VONAGE_API_KEY = process.env.VONAGE_API_KEY;
 const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET;
 const INSIGHT_NUMBER = process.env.INSIGHT_NUMBER;
+
+const { NumberInsightV2, Insight } = require('@vonage/number-insight-v2');
 
 const insight = new NumberInsightV2({
   apiKey: VONAGE_API_KEY,
