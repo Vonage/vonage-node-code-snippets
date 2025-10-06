0744d1280ebb026f0113b917ecdd53d39937a565
Chuck Reeves <manchuck@users.noreply.github.com>
2025-10-06 10:01:48 -0400

    feat: added new mms examples (#180)
    Co-authored-by: Karl Lingiah <karl@superchilled.co.uk>

---
+++ messages/mms/send-mms-file.js
@@ -0,0 +1,39 @@
+require('dotenv').config({ path: __dirname + '/../.env' });
+
+const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+const VONAGE_PRIVATE_KEY = process.env.VONAGE_PRIVATE_KEY;
+const MESSAGES_TO_NUMBER = process.env.MESSAGES_TO_NUMBER;
+const MMS_SENDER_ID = process.env.MMS_SENDER_ID;
+const MESSAGES_FILE_URL = process.env.MESSAGES_FILE_URL;
+const MESSAGES_API_URL = process.env.MESSAGES_API_URL;
+
+const { Vonage } = require('@vonage/server-sdk');
+const { Channels } = require('@vonage/messages');
+
+/**
+ * It is best to send messages using JWT instead of basic auth. If you leave out
+ * apiKey and apiSecret, the messages SDK will send requests using JWT tokens
+ *
+ * @link https://developer.vonage.com/en/messages/technical-details#authentication
+ */
+const vonage = new Vonage(
+  {
+    applicationId: VONAGE_APPLICATION_ID,
+    privateKey: VONAGE_PRIVATE_KEY,
+  },
+  {
+    ...(MESSAGES_API_URL ? {apiHost: MESSAGES_API_URL} : {}),
+  },
+);
+
+vonage.messages.send({
+  messageType: 'file',
+  channel: Channels.MMS,
+  file: {
+    url: MESSAGES_FILE_URL,
+  },
+  to: MESSAGES_TO_NUMBER,
+  from: MMS_SENDER_ID,
+})
+  .then(({ messageUUID }) => console.log(messageUUID))
+  .catch((error) => console.error(error));
