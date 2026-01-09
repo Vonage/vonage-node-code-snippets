91e259e2662fb3c988cda5bcb907b2891b78d264
Alvaro Navarro <alvaro.navarro@vonage.com>
2026-01-09 11:58:07 +0100

    fix: update channels in Verify request code snippet

--- verify2/send-request-with-fallback.js
+++ verify2/send-request-with-fallback.js
@@ -4,7 +4,6 @@ const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
 const VONAGE_APPLICATION_PRIVATE_KEY_PATH = process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH;
 const VERIFY_BRAND_NAME = process.env.VERIFY_BRAND_NAME;
 const VERIFY_NUMBER = process.env.VERIFY_NUMBER;
-const VERIFY_TO_EMAIL = process.env.VERIFY_TO_EMAIL;
 
 const { Vonage } = require('@vonage/server-sdk');
 const { Channels, SilentAuthChannel } = require('@vonage/verify2');
@@ -22,8 +21,12 @@ vonage.verify2.newRequest({
       to: VERIFY_NUMBER,
     },
     {
-      channel: Channels.EMAIL,
-      to: VERIFY_TO_EMAIL,
+      channel: Channels.SMS,
+      to: VERIFY_NUMBER,
+    },
+    {
+      channel: Channels.VOICE,
+      to: VERIFY_NUMBER,
     },
   ],
 })
