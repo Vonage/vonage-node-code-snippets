ec5b6b88a84f3e8d084dae76d423408ad0deff8c
Chuck Reeves <manchuck@users.noreply.github.com>
2026-02-18 08:22:52 -0500

    feat(messages): added typing indicator support (#190)

---
+++ messages/whatsapp/send-replying-indicator.js
@@ -0,0 +1,34 @@
+require('dotenv').config({ path: __dirname + '/../../.env' });
+
+const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+const VONAGE_PRIVATE_KEY = process.env.VONAGE_PRIVATE_KEY;
+const MESSAGES_MESSAGE_ID = process.env.MESSAGES_MESSAGE_ID;
+const GEOSPECIFIC_MESSAGES_API_URL = process.env.GEOSPECIFIC_MESSAGES_API_URL;
+
+const { Vonage } = require('@vonage/server-sdk');
+const { UpdateMessageStatus } = require('@vonage/messages');
+
+/**
+ * It is best to send messages using JWT instead of basic auth.
+ *
+ * @link https://developer.vonage.com/en/messages/technical-details#authentication
+ */
+const vonage = new Vonage(
+  {
+    applicationId: VONAGE_APPLICATION_ID,
+    privateKey: VONAGE_PRIVATE_KEY,
+  },
+  {
+    ...(GEOSPECIFIC_MESSAGES_API_URL ? { apiHost: GEOSPECIFIC_MESSAGES_API_URL } : {}),
+  },
+);
+
+vonage.messages.updateMessage(
+  MESSAGES_MESSAGE_ID,
+  UpdateMessageStatus.READ,
+  {
+    typingIndicator: 'text',
+  },
+)
+  .then(({ messageUUID }) => console.log(messageUUID))
+  .catch((error) => console.error(error));
