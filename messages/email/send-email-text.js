84073b1a3afc6579f138cf7efa3409ea7ae6f345
Chuck MANCHUCK Reeves <chuck@manchuck.com>
2026-06-29 14:18:25 -0400

    chore: added examples for email channels
    fix: example text for email
    fix: incorrect example text for html email
    
    Co-authored-by: Karl Lingiah <karl@superchilled.co.uk>

---
+++ messages/email/send-email-text.js
@@ -0,0 +1,39 @@
+require('dotenv').config({ path: __dirname + '/../../.env' });
+
+const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID;
+const VONAGE_PRIVATE_KEY = process.env.VONAGE_PRIVATE_KEY;
+const MESSAGES_TO_EMAIL = process.env.MESSAGES_TO_EMAIL;
+const EMAIL_SENDER_ID = process.env.EMAIL_SENDER_ID;
+const MESSAGES_API_URL = process.env.MESSAGES_API_URL;
+
+const { Vonage } = require('@vonage/server-sdk');
+const { Channels, MessageType } = require('@vonage/messages');
+
+/**
+ * It is best to send messages using JWT instead of basic auth. If you leave out
+ * apiKey and apiSecret, the messages SDK will send requests using JWT tokens
+ *
+ * @link https://developer.vonage.com/en/messages/technical-details#authentication
+ */
+const vonage = new Vonage(
+  {
+    applicationId: VONAGE_APPLICATION_ID,
+    privateKey: VONAGE_PRIVATE_KEY,
+  },
+  {
+    ...(MESSAGES_API_URL ? { apiHost: MESSAGES_API_URL } : {}),
+  },
+);
+
+vonage.messages.send({
+  to: MESSAGES_TO_EMAIL,
+  from: EMAIL_SENDER_ID,
+  messageType: MessageType.HTML,
+  channel: Channels.EMAIL,
+  text: 'This is an Email text message sent via the Vonage Messages API.',
+  email: {
+    subject: 'Your message subject'
+  }
+})
+  .then(({ messageUUID }) => console.log(messageUUID))
+  .catch((error) => console.error(error));
